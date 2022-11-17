c1fea7d7cc1315a6211ed90106c7618fdf05152b
草鞋没号 <308487730@qq.com>
2022-11-17 14:47:03 +0800

    feat: electron-forge-template-vite@0.0.1

---
+++ vite/src/ViteTemplate.ts
@@ -0,0 +1,57 @@
+import path from 'path';
+
+import { ForgeListrTaskDefinition, InitTemplateOptions } from '@electron-forge/shared-types';
+import { BaseTemplate } from '@electron-forge/template-base';
+import fs from 'fs-extra';
+
+class ViteTemplate extends BaseTemplate {
+  public templateDir = path.resolve(__dirname, '..', 'tmpl');
+
+  public async initializeTemplate(directory: string, options: InitTemplateOptions): Promise<ForgeListrTaskDefinition[]> {
+    const superTasks = await super.initializeTemplate(directory, options);
+    return [
+      ...superTasks,
+      {
+        title: 'Setting up Forge configuration',
+        task: async () => {
+          await this.copyTemplateFile(directory, 'forge.config.js');
+        },
+      },
+      {
+        title: 'Setting up vite configuration',
+        task: async () => {
+          await this.copyTemplateFile(directory, 'vite.main.config.js');
+          await this.copyTemplateFile(directory, 'vite.renderer.config.js');
+          await this.copyTemplateFile(path.join(directory, 'src'), 'renderer.js');
+          await this.copyTemplateFile(path.join(directory, 'src'), 'preload.js');
+
+          await this.updateFileByLine(
+            path.resolve(directory, 'src', 'index.js'),
+            (line) => {
+              if (line.includes('mainWindow.loadFile')) return '  mainWindow.loadURL(MAIN_WINDOW_VITE_ENTRY);';
+              if (line.includes('preload: ')) return '      preload: MAIN_WINDOW_PRELOAD_VITE_ENTRY,';
+              return line;
+            },
+            path.resolve(directory, 'src', 'main.js')
+          );
+
+          await this.updateFileByLine(path.resolve(directory, 'src', 'index.html'), (line) => {
+            if (line.includes('link rel="stylesheet"')) return '';
+            if (line.includes('</body>')) return '    <script type="module" src="/renderer.js"></script>\n  </body>';
+            return line;
+          });
+
+          // update package.json entry point
+          const pjPath = path.resolve(directory, 'package.json');
+          const currentPJ = await fs.readJson(pjPath);
+          currentPJ.main = '.vite/main';
+          await fs.writeJson(pjPath, currentPJ, {
+            spaces: 2,
+          });
+        },
+      },
+    ];
+  }
+}
+
+export default new ViteTemplate();
---
+++ vite/tmpl/forge.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  packagerConfig: {},
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin'],
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {},
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {},
+    },
+  ],
+  plugins: [
+    {
+      name: '@electron-forge/plugin-vite',
+      config: {
+        main: {
+          config: './vite.main.config.js',
+        },
+        renderer: {
+          config: './vite.renderer.config.js',
+          entryPoints: [
+            {
+              html: './src/index.html',
+              name: 'main_window',
+              preload: {
+                js: './src/preload.js',
+              },
+            },
+          ],
+        },
+      },
+    },
+  ],
+};
